42e44e113cc6e16ba2259d622c9b16c808bac651
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-01-17 17:19:16 +0100

    cleand up code and added comments

--- backend/js/fetchApi.js
+++ backend/js/fetchApi.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 /**@param id:
- * take the id of a moive, if not filled in then all movies will be fetched
+ * takes the id of a moive, if not filled in then all movies will be fetched
  * */
 async function fetchMovieApi(id = null) {
   const uri =
@@ -10,8 +10,7 @@ async function fetchMovieApi(id = null) {
       : `https://lernia-kino-cms.herokuapp.com/api/movies/${id}`;
 
   const res = await fetch(uri);
-  const movies = await res.json();
-  return movies;
+  return res.json();
 }
 
 export { fetchMovieApi as movies };
