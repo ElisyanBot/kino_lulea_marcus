139aa561e54b202224b8448cf4724a2c55caf4c6
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-01-19 19:08:29 +0100

    changed code from res.send to res.render for temp hbs

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 import express, { response } from "express";
 import { readFile } from "fs";
 import { movies } from "./backend/js/fetchApi.js";
+import { engine } from "express-handlebars";
 
 const app = express();
-app.get("/", (req, res) => {
-  readFile("./static/index.html", "utf8", (error, data) => {
-    !error
-      ? res.status(200).send(data)
-      : console.log("ERR: could not find local file");
-  });
-});
-
-app.get("/movies", (req, res) => {
-  movies().then((data) => res.status(200).send(data));
-});
-
-app.get("/movies/:movieId", (req, res) => {
-  movies(req.params.movieId).then((data) => res.status(200).send(data));
-});
 
 app.use(express.static("./static"));
 
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "index",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("homepage", {});
+});
+
+app.get("/movies", (req, res) => {
+  res.render("allMovies", {});
+  // movies().then((movieArr) => res.status(200).send(movieArr.data));
+});
+
+app.get("/movies/:movieId", (req, res) => {
+  res.render("movie", {});
+  // movies(req.params.movieId).then((movie) => res.status(200).send(movie.data));
+});
+
+app.all("*",(req, res) =>{
+  res.render("404page",{message: "not found"})
+})
+
 app.listen(5080, () => {
-  console.log(" \n ============ server is upp and running ============= ");
+  console.log(" \n ============ server is running ============= ");
 });
