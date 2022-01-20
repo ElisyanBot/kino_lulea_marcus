4dcb8d9c8ed18152cf30c4112c48ca486b0e58eb
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-01-20 17:06:17 +0100

    added dynamic content to hbs

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express, { response } from "express";
-import { readFile } from "fs";
+import express from "express";
 import { movies } from "./backend/js/fetchApi.js";
 import { engine } from "express-handlebars";
+import path from "path";
 
 const app = express();
 
-app.use(express.static("./static"));
 
 app.set("view engine", "hbs");
 app.engine(
@@ -14,25 +13,30 @@ app.engine(
     extname: "hbs",
     defaultLayout: "index",
   })
-);
+  );
+  
+app.use("/", express.static("./static"));
 
 app.get("/", (req, res) => {
   res.render("homepage", {});
 });
 
 app.get("/movies", (req, res) => {
-  res.render("allMovies", {});
-  // movies().then((movieArr) => res.status(200).send(movieArr.data));
-});
-
+  movies().then((movieArr) => res.render(
+    "allMovies", 
+    { movies: movieArr.data}
+    ));
+  });
+  
+app.use("/movies", express.static("./static"));
 app.get("/movies/:movieId", (req, res) => {
-  res.render("movie", {});
-  // movies(req.params.movieId).then((movie) => res.status(200).send(movie.data));
+  movies(req.params.movieId).then((movie) => res.render("movie", {movie : movie.data.attributes}));
 });
 
 app.all("*",(req, res) =>{
-  res.render("404page",{message: "not found"})
+  res.render("404page",{message: "404: not found"})
 })
+
 
 app.listen(5080, () => {
   console.log(" \n ============ server is running ============= ");
